044029792c6547a0a4d08b1f76215e7957b3d4c1
Justin Moore <jmoore315@gmail.com>
2014-09-08 21:43:56 -0500

    Backend of extention more or less completed

--- switchmark.js
+++ switchmark.js
@@ -1,183 +1,54 @@
-
-
-function printBookmarks(bookmarks) {
-  if(bookmarks) {
-    bookmarks.forEach(function(bookmark) {
-      if (bookmark.children) {
-        if(bookmark.parentId == "2"){
-          console.debug(bookmark.id + ' - ' + bookmark.title);
-          //folders.append(bookmark);
-        }
-        printBookmarks(bookmark.children);
-      }
-    });
-  }
-}
-
-
-chrome.bookmarks.getTree(function(bookmarks) {
-  printBookmarks(bookmarks);
-});
-
-
-// Traverse the bookmark tree, and print the folder and nodes.
-function dumpBookmarks() {
-  chrome.bookmarks.getTree(function(bookmarks) {
-    printBookmarks(bookmarks);
-  });
-}
-
-function printBookmarkBar() {
-
-}
-
-function showFolders() {
-  $('#bookmark-bars').text("Hello!");
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  dumpBookmarks();
-});
-
-
-//Print bookmark bar:
-chrome.bookmarks.getSubtree("1", function(bookmarks) {
-  printBookmarks(bookmarks);
-});
-
-
-function findOrCreateSwitchmarkFolder() {
-  var folders = [];
-  //Switchmark folder should be placed in 'Other bookmarks folder'
-  chrome.bookmarks.getSubtree("2", function(bookmarks) {
-    bookmarks.forEach(function(bookmark) {
-      if(bookmark.children) {
-        folders.append(bookmark);
-      }
-    });
-  });
-}
-
-folders.forEach(function(bookmark) {
-    console.debug(bookmark.id + ' - ' + bookmark.title);
-  });
-
-
-chrome.bookmarks.getSubtree("2", function(bookmarks) {
-  for(i = 0; i< bookmarks.length; i++){
-    if(bookmark.children) {
-      console.debug(bookmark.id + ' - ' + bookmark.title);
-    }
-  }
-});
-
-
-function printBookmarks(bookmarks) {
-  for(i = 0; i< bookmarks.length; i++){
-    if(bookmark.children) {
-      console.debug(bookmark.id + ' - ' + bookmark.title);
-    }
-  }
-}  
-
-chrome.bookmarks.getSubtree("2", printBookmarks);
-
-
-function print5(bookmarks) {
-  console.debug("5");
-}
-
-
-/////
-
-
-
-function printBookmarkFolders(bookmarks) {
-  if(bookmarks) {
-    bookmarks.forEach(function(bookmark) {
-      if (bookmark.children) {
-        console.debug(bookmark.id + ' - ' + bookmark.title);
-      }
-    });
-  }
-}
-
-function isFolder(bookmark) {
-  bookmark.getChildren(function(children) {
-    return (children.length > 0);
-  });
-}
-
-function printBookmarkFolders(bookmarks) {
-  for(i = 0; i < bookmarks.length; i++) {
-    if(isFolder(bookmarks[i])){
-      console.debug(bookmarks[i].id + ' - ' + bookmarks[i].title);
-    }
-  }  
-}
-
-
-
-
-
-function printBookmarks(bookmarks) {
-  if(bookmarks) {
-    bookmarks.forEach(function(bookmark) {
-      //if (bookmark.children) {
-        console.debug(bookmark.id + ' - ' + bookmark.title);
-      //}
-    });
-  }
-}
-
-function printBookmarksHelper(bookmarks) {
-  if(bookmarks) {
-    bookmarks.forEach(function(bookmark) {
-      if(bookmark.children){
-        console.debug(bookmark.id + ' - ' + bookmark.title);
-      }
-      // if (bookmark.children) {
-      //   printBookmarks(bookmark.children);
-      //   console.debug(bookmark.id + ' - ' + bookmark.title);
-      // }
-    });
-  }
-}
-chrome.bookmarks.getChildren("2", function(bookmarks) {
-  printBookmarksHelper(bookmarks);
-});
-
-
-chrome.bookmarks.b
-
-/* The good stuff: */
-
 function clearBookmarkBar() {
   chrome.bookmarks.getChildren("1", function(bookmarks) {
     bookmarks.forEach(function(bookmark) {
-      if(bookmark.children){
-        chrome.bookmarks.removeTree(bookmark.id);
-      }
-      else {
-        chrome.bookmarks.remove(bookmark.id);
-      }
+      chrome.bookmarks.removeTree(bookmark.id); 
+      //chrome.bookmarks.remove(bookmark.id);
     });
   });
 }
 
 function copyFromFolderToFolder(fromBookmarkFolderId, toBookmarkFolderId) {
+  if(toBookmarkFolderId == "1") {
+    clearBookmarkBar();
+  }
   chrome.bookmarks.getChildren(fromBookmarkFolderId, function(bookmarks) {
     bookmarks.forEach(function(bookmark) {
       //Create a copy of the bookmark and place it under the appropriate bookmark bar folder
       var bm = chrome.bookmarks.create({'parentId': toBookmarkFolderId, 'title': bookmark.title, 'url': bookmark.url}, function(newBookmark){
-        if(bookmark.children){
-          copyFromFolderToFolder(bookmark.id, newBookmark.id);
-        }
+        console.debug("New id is: " + newBookmark.id + " Title is: " + newBookmark.title);
+        console.debug("Copying folder. New id is: " + newBookmark.id);
+        copyFromFolderToFolder(bookmark.id, newBookmark.id);
       });
     });
   });
 }
 
-clearBookmarkBar();
+function displaySwitchmarkFolders(bookmarkNodes, parentIsSwitchmarkFolder) {
+  if(parentIsSwitchmarkFolder) {
+    bookmarkNodes.forEach(function(bookmark) {
+      $('#bookmark-bars').append($("<p class='bookmark-folder' id=" + bookmark.id + ">" + bookmark.title + "</p>"));
+    });
+  }
+  else if(bookmarkNodes) {
+    bookmarkNodes.forEach(function(bookmark) {
+      if(bookmark.title == "Switchmark") {
+        displaySwitchmarkFolders(bookmark.children, true);
+      }
+      else {
+        displaySwitchmarkFolders(bookmark.children, false);
+      }
+    });
+  }
+}
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.bookmarks.getTree(function(bookmarkNodes){
+    displaySwitchmarkFolders(bookmarkNodes, false);
+  });
+  $('body').on('click', '.bookmark-folder', function() {
+    copyFromFolderToFolder($(this).attr('id'), "1");
+});
+  //check for no content in switchmark folder to display info
+});
